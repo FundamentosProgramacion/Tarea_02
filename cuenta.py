a68190fb871928b6ce80a5f2ea37d1a4bad8770c
Sebastian Morales <sebfut@hotmail.com>
2018-01-22 15:34:01 -0600

    Tarea_02

--- cuenta.py
+++ cuenta.py
@@ -1,7 +1,22 @@
+#----------------------------------------------------------------------------------------------------------------------
+
 #encoding: UTF-8
 
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor: Sebastian Morales Martin, A01376228
+# Descripcion: Calculo de total de una cuenta de restaurante considerando IVA y Propina
 
-# A partir de aquí escribe tu programa
+# Análisis
+# Entrada: Subtotal de la cuenta
+# Salida: Total de la cuenta con el IVA y la propina
+# Relación E/S: el calculo del cubtotal para obtener el IVA y la propina
 
+#----------------------------------------------------------------------------------------------------------------------
+
+subT = float(input("Subtotal de la cuenta: "))
+
+propela = subT * 0.12
+iva = subT * 0.16
+
+total = subT + propela + iva
+
+print("Subtotal: %10.2f \nIVA: %10.2f \nPropina: %10.2f \nTOTAL: %10.2f" % (subT, iva, propela, total))
