c45bbd75f6c4c5ac1165f64e8e6e085a611e6812
FernandoSSilvaM <35341047+FernandoSSilvaM@users.noreply.github.com>
2018-01-22 12:10:52 -0600

    Complatado

--- cuenta.py
+++ cuenta.py
@@ -1,7 +1,13 @@
 #encoding: UTF-8
 
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+# Autor:Fernando Sebastian Silva Miramontes, A01746925
+# Descripcion: Caclular el costo total de una comida que incluya la propina y el IVA
 
-# A partir de aquí escribe tu programa
-
+subtotal = float(input("Cuanto le costo la comida?\n"))
+iva = subtotal*0.15
+propina = subtotal*0.13
+costoTotal = subtotal+iva+propina
+print("Costo de su comida: ",subtotal)
+print("IVA: ",iva)
+print("Propina: ",propina)
+print("Costo Total: ",costoTotal)
