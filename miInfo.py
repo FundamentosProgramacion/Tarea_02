35587a74afaed07b38db6ee83bf96a32e30e6fe3
Andrés Reyes <35356747+AndresReyesRangel@users.noreply.github.com>
2018-01-23 20:37:44 -0600

    Update miInfo.py

--- miInfo.py
+++ miInfo.py
@@ -1,8 +1,18 @@
 #encoding: UTF-8
 
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
-
+# Autor: Andrés Reyes Rangel
+# Descripcion: Imprimir mi información.
 # A partir de aquí escribe tu programa
 
-print("rmr")
+nombre="Andrés Reyes Rangel"
+matricula="A01746592"
+carrera="ISC"
+escuela="Indoamericano"
+descripcion="Me gusta jugar futbol(soccer) asi como verlo y me equipo favorito de México es Pumas(UNAM).\n" \
+            "\t \t tambien me gusta mucho salir con amigos a una fiesta o simplemente a jugar videojuego o platicar " \
+
+print("Nombre: ", nombre)
+print("Matricula:", matricula)
+print("Carrera que está estudiando:", carrera)
+print("Escuela de procedencia:", escuela)
+print("Descripcion:", descripcion)
