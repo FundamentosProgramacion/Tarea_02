21ac92536403f4670ff08e0b29f0180e8414b7a5
GuillermoAdrianU <35458662+GuillermoAdrianU@users.noreply.github.com>
2018-01-23 21:56:08 -0600

    Update cordenadas.py

--- cordenadas.py
+++ cordenadas.py
@@ -9,7 +9,7 @@ x2 = int(input("Teclea la cordenada x2: "))
 y1 = int(input("Teclea la cordenada y1: "))
 y2 = int(input("Teclea la cordenada y2: "))
 
-Distancia = sqtr(((x2-x1)**2)+((y2-y1)**2)))
+Distancia = sqrt(((x2-x1)**2)+((y2-y1)**2)))
 
 print("La distancia es: ", format(Distancia".4"))
 
