d3dc3c6b0e826c143b68ec8efa3918d288571ca6
GOD <A01375315@itesm.mx>
2018-01-23 19:05:54 -0600

    Tarea 02

--- cuenta.py
+++ cuenta.py
@@ -1,7 +1,14 @@
 #encoding: UTF-8
+# Autor: Genaro Ortiz Durán, A01375315
+# Descripcion: Calcular el costo total de una comida.
 
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
+totalComida=int(input("¿Cuál fue el total de la comida?:"))
+propina= totalComida*0.13
+IVA=totalComida*0.15
+total=totalComida+propina+IVA
 
-# A partir de aquí escribe tu programa
+print("Costo por la comida:",totalComida,"$")
+print("Propina:","$",format(propina,".2f"))
+print("IVA:","$",format(IVA,".2f"))
+print("Costo total de la comida:","$",format(total,".2f"))
 
--- miInfo.py
+++ miInfo.py
@@ -1,8 +1,15 @@
 #encoding: UTF-8
+# Autor: Genaro Ortiz Durán, A01375315
+# Descripcion: Hacer un programa que escriba la información sobre mí.
 
-# Autor: tuNombreCompleto, tuMatricula
-# Descripcion: Texto que describe en pocas palabras el problema que estás resolviendo.
-
-# A partir de aquí escribe tu programa
-
-print("rmr")
+print("Nombre:")
+print("Genaro Ortiz Durán")
+print("Matricula:")
+print("A01375315")
+print("Carrera:")
+print("ISDR")
+print("Carrera de procedencia:")
+print("Prepa Tec, programa bicultural")
+print("Información:")
+print("Me gusta aprender nuevas cosas, me gusta pasar tiempos con mis amigos, hacer deportes de los cuales mi favorito es el soccer")
+print("Me gusta encontrar el origen y el significado de las cosas y todo lo que este relacionado con la tecnología")
