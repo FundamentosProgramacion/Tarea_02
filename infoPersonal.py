60983168a5120a44108788fc057ae0652a561ae7
Jean Paul Esquivel Lobato <30931626+JeanPaulLobat@users.noreply.github.com>
2018-01-25 08:25:15 -0600

    Primer parte de la tarea
    Es una parte de la tarea profe, solo faltan dos ejercicios por terminar.

---
+++ infoPersonal.py
@@ -0,0 +1,25 @@
+#  Autor: Jean Paul Esquivel Lobato    A01376152
+# Descripcion: Mi información básica
+
+
+print("Nombre:")
+
+print("Jean PauL Esquivel Lobato")
+
+print("Matrícula:")
+
+print("A01376152")
+
+print("Carrera:")
+
+print ("ISDR")
+
+print("Escuela de procedencia:")
+
+print ("Prepa Tec, programa Bicultural")
+
+print("Descripcción:")
+
+print ("""Me gusta jugar futbol y practicar mi profesion de dj. 
+También dibujo y tocando instrumentos 
+como guitarra, piano y trompeta. Me encanta bailar y hacer feliz a las personas.""")
